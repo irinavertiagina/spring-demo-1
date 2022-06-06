c2b015b7b995830b5e99815d246af3ddc422b3f4
irinavertiagina <irkinson9@gmail.com>
2022-06-06 17:30:22 -0400

    dependency injection through ctor in xml config. (musicbean injected to playerbean)

--- src/main/java/ru/irina/spring/Driver.java
+++ src/main/java/ru/irina/spring/Driver.java
@@ -6,9 +6,9 @@ public class Driver {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
-
-       Player player = new Player(music);
+        //Music music = context.getBean("musicBean", Music.class);
+        //Player player = new Player(music);
+        Player player = context.getBean("playerBean", Player.class);
         player.play();
         context.close();
     }
