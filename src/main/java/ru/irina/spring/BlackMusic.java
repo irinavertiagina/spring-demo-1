db7346d0e32ff631b70fbf14cde6f998accba5a0
irinavertiagina <irkinson9@gmail.com>
2022-06-07 17:05:08 -0400

    bean scope singlelton/prototype, init and destroy methods

--- src/main/java/ru/irina/spring/BlackMusic.java
+++ src/main/java/ru/irina/spring/BlackMusic.java
@@ -6,4 +6,10 @@ public class BlackMusic implements Music {
     public String getSong() {
         return "Hvis Lys Tar Osset";
     }
+    public void doInit(){
+        System.out.println("Doing my initialization...");
+    }
+    public void doDestroy(){
+        System.out.println("Doing my destruction...");
+    }
 }
